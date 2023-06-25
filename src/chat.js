9950b39c87cb1c025821d55088b93b9e2def92f7
Zoe Nolan <github@zoenolan.org>
2023-06-25 17:53:13 +0100

    Add chatbot class

--- src/chat.js
+++ src/chat.js
@@ -5,7 +5,7 @@
 require("dotenv").config();
 
 const readline = require("readline");
-const chatbot = require("./chatbot");
+const chatbot = require("./chatbot.js");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -37,15 +37,16 @@ const isExitString = function (input) {
 async function main() {
   console.log("\n Use 'quit' or 'exit' to leave the bot\n");
 
+  const bot = new chatbot.Chatbot();
+
   let done = false;
-
   while (!done) {
-    const inputString = await getInput();
+    const input = await getInput();
 
-    done = isExitString(inputString);
+    done = isExitString(input);
 
     if (!done) {
-      const response = inputString;
+      const response = bot.process(input);
 
       console.log(response);
     }
